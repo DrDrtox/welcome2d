070e93f2fd230cb7701084fdc7185ef32ebefda0
drtox <45079652+drtox@users.noreply.github.com>
2019-01-17 19:23:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js'),
     bot = new Discord.Client({sisableEveryone: true})
     console.log("muuuuuuuuute,");
 bot.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("533248398523105281");
+const mohamed= member.guild.channels.get("535461700225204235");
 if(!mohamed) return;
 if(mohamed) {
 setTimeout(() => mohamed.send(`**:scroll: . Welcome *``2`` Nautica , :coffee: **`), 4000)        
